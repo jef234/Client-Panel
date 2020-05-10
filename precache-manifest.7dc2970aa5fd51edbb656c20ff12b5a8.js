0806330d0e79ab2db1e5852c8a150dd95b8c207a
jr051323 <jeffrey.rajkumar@cerner.com>
2020-05-10 23:38:08 +0530

    Updates

---
+++ precache-manifest.7dc2970aa5fd51edbb656c20ff12b5a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0c7c43f67d29b44927937906b27d6d",
+    "url": "/jef234/Client-Panel/index.html"
+  },
+  {
+    "revision": "bf8df4d9acf1b0875f0d",
+    "url": "/jef234/Client-Panel/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "9e62a81940725af4b61d",
+    "url": "/jef234/Client-Panel/static/js/2.e26546d9.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/jef234/Client-Panel/static/js/2.e26546d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf8df4d9acf1b0875f0d",
+    "url": "/jef234/Client-Panel/static/js/main.3ecdd6b5.chunk.js"
+  },
+  {
+    "revision": "234d14daf546069dbbcf",
+    "url": "/jef234/Client-Panel/static/js/runtime-main.fcce2ddd.js"
+  }
+]);
