34d35ec7cafffc0f7960fa733f3863a3c28a31f3
jr051323 <jeffrey.rajkumar@cerner.com>
2020-05-14 23:45:27 +0530

    Added amount edit and client delete functionality

--- src/components/clients/ClientDetails.js
+++ src/components/clients/ClientDetails.js
@@ -8,8 +8,51 @@ import Spinner from '../layouts/Spinner'
 import classnames from 'classnames'
 
 class ClientDetails extends Component {
+    state = {
+        showBalanceUpdate: false,
+        balanceUpdateAmount: ''
+    }
+
+    balanceSubmit = e => {
+        e.preventDefault()
+
+        const { client, firestore } = this.props
+        const { balanceUpdateAmount } = this.state
+
+        const clientUpdate = {
+            balance: parseFloat(balanceUpdateAmount)
+        }
+        firestore.update({ collection: 'clients', doc: client.id }, clientUpdate)
+    }
+
+    onChange = e => this.setState({ [e.target.name]: e.target.value })
+    onDelete = () => {
+        const { client, firestore, history } = this.props
+
+        firestore
+            .delete({ collection: 'clients', doc: client.id })
+            .then(history.push('/'))
+    }
+
     render() {
         const { client } = this.props
+        const { showBalanceUpdate, balanceUpdateAmount } = this.state
+
+        let balanceForm = ''
+        if (showBalanceUpdate) {
+            balanceForm = (
+                <form onSubmit={this.balanceSubmit}>
+                    <div className="input-group">
+                        <input type="number" required className="form-control" name="balanceUpdateAmount" placeholder="Add New Balance" value={balanceUpdateAmount} onChange={this.onChange} />
+                        <div className="input-group-append">
+                            <input type="submit" value="Update" className="btn btn-outline-dark" />
+                        </div>
+                    </div>
+                </form>
+            )
+        } else {
+            balanceForm = null
+        }
 
         if (client) {
             return (
@@ -23,7 +66,7 @@ class ClientDetails extends Component {
                         <div className="col-6">
                             <div className="btn-group float-right">
                                 <Link to={`/client/edit/${client.id}`} className="btn btn-sm btn-dark">Edit</Link>
-                                <button className="btn btn-sm btn-danger">Delete</button>
+                                <button onClick={this.onDelete} className="btn btn-sm btn-danger">Delete</button>
                             </div>
                         </div>
                     </div>
@@ -39,12 +82,20 @@ class ClientDetails extends Component {
                                 </div>
                                 <div className="col-6">
                                     <h4 className="pull-right">
+                                        Balance:
                                         <span className={classnames({
-                                            'text-danger': client.balance > 0,
-                                            'text-success':client.balance <= 0
-                                        })}>
-                                        Balance: ${parseFloat(client.balance).toFixed(2)}</span> 
+                                        'text-danger': client.balance > 0,
+                                        'text-success': client.balance <= 0
+                                    })}>
+                                            {' '}${parseFloat(client.balance).toFixed(2)}{' '}
+                                        </span>
+                                        <small>
+                                            <a href=" " onClick={(e) => { this.setState({ showBalanceUpdate: !this.state.showBalanceUpdate }); e.preventDefault() }}>
+                                                <i className="fas fa-pencil-alt" style={{}}></i>
+                                            </a>
+                                        </small>
                                     </h4>
+                                    {balanceForm}
                                 </div>
                             </div>
                             <hr />
