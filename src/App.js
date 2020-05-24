3323920047796273ba5aa08e2fb3efc44d511f60
jr051323 <jeffrey.rajkumar@cerner.com>
2020-05-24 18:46:26 +0530

    Auth added with router guard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { UserIsAuthenticated, UserIsNotAuthenticated } from './components/helpers/auth'
 
 import { Provider } from 'react-redux'
-import {store, rrfProps} from './store'
-import { ReactReduxFirebaseProvider} from 'react-redux-firebase'
+import { store, rrfProps } from './store'
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
 
 import AppNavbar from './components/layouts/AppNavbar'
 import Dashboard from './components/layouts/Dashboard'
 import AddClient from './components/clients/AddClient'
 import EditClient from './components/clients/EditClient'
 import ClientDetails from './components/clients/ClientDetails'
+import Login from './components/auth/Login'
 
 import './App.css';
 
@@ -19,19 +21,20 @@ function App() {
   return (
     <Provider store={store}>
       <ReactReduxFirebaseProvider {...rrfProps}>
-      <Router>
-        <div className="App">
-          <AppNavbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Dashboard} />
-              <Route exact path="/client/add" component={AddClient} />
-              <Route exact path="/client/edit/:id" component={EditClient} />
-              <Route exact path="/client/:id" component={ClientDetails} />
-            </Switch>
+        <Router>
+          <div className="App">
+            <AppNavbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={UserIsAuthenticated(Dashboard)} />
+                <Route exact path="/client/add" component={UserIsAuthenticated(AddClient)} />
+                <Route exact path="/client/edit/:id" component={UserIsAuthenticated(EditClient)} />
+                <Route exact path="/client/:id" component={UserIsAuthenticated(ClientDetails)} />
+                <Route exact path="/login" component={UserIsNotAuthenticated(Login)} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
       </ReactReduxFirebaseProvider>
     </Provider>
   );
--- src/components/layouts/AppNavbar.js
+++ src/components/layouts/AppNavbar.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firebaseConnect } from 'react-redux-firebase'
 
 class AppNavbar extends Component {
+    state = {
+        isAuthenticated: true
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        const { auth } = props
+
+        if (auth.uid) {
+            return { isAuthenticated: true }
+        } else {
+            return { isAuthenticated: false }
+        }
+    }
+
+    onLogoutClick = e => {
+        e.preventDefault()
+
+        const { firebase } = this.props
+        firebase.logout()
+    }
+
     render() {
+        const { isAuthenticated } = this.state
+        const { auth } = this.props
         return (
             <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
                 <Link to="/" className="navbar-brand" href="#">Client Panel</Link>
@@ -10,15 +37,37 @@ class AppNavbar extends Component {
                     <span className="navbar-toggler-icon" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link to="/" className="nav-link">Dashboard</Link>
-                        </li>
-                    </ul>
+                    {isAuthenticated ? (<React.Fragment>
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link">Dashboard</Link>
+                            </li>
+                        </ul>
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <span className="nav-link">{auth.email}</span>
+                            </li>
+                            <li className="nav-item">
+                                <a href="#!" onClick={this.onLogoutClick} className="nav-link">Logout</a>
+                            </li>
+                        </ul>
+                    </React.Fragment>
+                    ) : null}
+
                 </div>
             </nav>
         )
     }
 }
 
-export default AppNavbar
+AppNavbar.protoTypes = {
+    firebase: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired
+}
+
+export default compose(
+    firebaseConnect(),
+    connect((state, props) => ({
+        auth: state.firebase.auth
+    }))
+)(AppNavbar)
