074f70641dc6af696aa4dc3ca03defab4cb8945d
jr051323 <jeffrey.rajkumar@cerner.com>
2020-05-25 14:11:39 +0530

    Configured for Firestore hosting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { UserIsAuthenticated, UserIsNotAuthenticated } from './components/helpers/auth'
 
 import { Provider } from 'react-redux'
